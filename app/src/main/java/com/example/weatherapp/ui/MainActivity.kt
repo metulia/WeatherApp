c67ada9bea79cabbc7c02aa997a9ae6611da3332
Yuliia Stepkina <metulia@mail.ru>
2023-03-07 10:58:02 +0300

    added fragment_main.xml, added MainFragment.kt, added viewBinding

---
+++ app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.weatherapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commit()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/ui/MainFragment.kt
@@ -0,0 +1,77 @@
+package com.example.weatherapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherapp.databinding.FragmentMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    protected val binding get() = _binding!!
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        //return inflater.inflate(R.layout.fragment_main, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val observer = object : Observer<AppState> {
+            override fun onChanged(data: AppState) {
+                renderData(data)
+            }
+        }
+
+        viewModel.getData().observe(viewLifecycleOwner, observer)
+
+        viewModel.getWeather()
+    }
+
+    private fun renderData(data: AppState) {
+        when (data) {
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                //binding.message.text = "Не получилось ${data.error}"
+                Snackbar.make(binding.mainView, "Не получилось ${data.error}", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+            }
+            is AppState.Success -> {
+                binding.loadingLayout.visibility = View.GONE
+                binding.cityName.text = data.weatherData.city.name
+                binding.temperatureValue.text = data.weatherData.temperature.toString()
+                binding.feelsLikeValue.text = data.weatherData.feelsLike.toString()
+                binding.cityCoordinates.text =
+                    "${data.weatherData.city.lat} ${data.weatherData.city.lon}"
+                Snackbar.make(binding.mainView, "Получилось", Snackbar.LENGTH_LONG).show()
+                //Toast.makeText(requireContext(), "Работает", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+
+}
