2d032cfb35827eec57f3604f6cdae346ed928703
Yuliia Stepkina <metulia@mail.ru>
2023-03-10 17:47:14 +0300

    added plugin 'kotlin-android-extensions'

--- app/src/main/java/com/example/weatherapp/domain/Weather.kt
+++ app/src/main/java/com/example/weatherapp/domain/Weather.kt
@@ -1,14 +1,19 @@
 package com.example.weatherapp.domain
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Weather(
     val city: City = getDefaultCity(),
     val temperature: Int = 0,
     val feelsLike: Int = 0
-)
+) : Parcelable
 
 fun getDefaultCity() = City("Москва", 55.755826, 37.617299900000035)
 
-data class City(val name: String, val lat: Double, val lon: Double)
+@Parcelize
+data class City(val name: String, val lat: Double, val lon: Double) : Parcelable
 
 fun getWorldCities(): List<Weather> {
     return listOf(
@@ -24,13 +29,16 @@ fun getWorldCities(): List<Weather> {
         Weather(City("Пекин", 39.90419989999999, 116.40739630000007), 19, 20)
     )
 }
+
 fun getRussianCities(): List<Weather> {
     return listOf(
         Weather(City("Москва", 55.755826, 37.617299900000035), 1, 2),
         Weather(City("Санкт-Петербург", 59.9342802, 30.335098600000038), 3, 3),
         Weather(City("Новосибирск", 55.00835259999999, 82.93573270000002), 5, 6),
-        Weather(City("Екатеринбург", 56.83892609999999, 60.60570250000001), 7,
-            8),
+        Weather(
+            City("Екатеринбург", 56.83892609999999, 60.60570250000001), 7,
+            8
+        ),
         Weather(City("Нижний Новгород", 56.2965039, 43.936059), 9, 10),
         Weather(City("Казань", 55.8304307, 49.06608060000008), 11, 12),
         Weather(City("Челябинск", 55.1644419, 61.4368432), 13, 14),
