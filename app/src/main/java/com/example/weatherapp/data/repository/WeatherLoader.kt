4b628251bed7eb415925f291136a1001115c7ef4
Yuliia Stepkina <metulia@mail.ru>
2023-03-21 13:06:05 +0300

    started lesson 6: created DetailsService.kt, added service to Manifest, started service in DetailsFragment.kt, created Local BroadcastReceiver

--- app/src/main/java/com/example/weatherapp/data/repository/WeatherLoader.kt
+++ app/src/main/java/com/example/weatherapp/data/repository/WeatherLoader.kt
@@ -6,9 +6,13 @@ import android.util.Log
 import com.example.weatherapp.BuildConfig
 import com.example.weatherapp.data.weather_dto.WeatherDTO
 import com.example.weatherapp.domain.OnServerResponse
+import com.example.weatherapp.ui.details.YANDEX_API_KEY
+import com.example.weatherapp.ui.details.YANDEX_DOMAIN
+import com.example.weatherapp.ui.details.YANDEX_PATH
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import java.io.BufferedReader
+import java.io.FileNotFoundException
 import java.io.InputStreamReader
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
@@ -17,14 +21,14 @@ class WeatherLoader(private val onServerResponseListener: OnServerResponse) {
 
     fun loadWeather(lat: Double, lon: Double) {
 
-        val urlText = "https://api.weather.yandex.ru/v2/informers?lat=${lat}&lon=${lon}"
+        val urlText = "$YANDEX_DOMAIN${YANDEX_PATH}lat=${lat}&lon=${lon}"
         val uri = URL(urlText)
         val urlConnection: HttpsURLConnection =
             (uri.openConnection() as HttpsURLConnection).apply {
                 connectTimeout = 1000
                 readTimeout = 1000
                 requestMethod = "GET"
-                addRequestProperty("X-Yandex-API-Key", BuildConfig.WEATHER_API_KEY)
+                addRequestProperty(YANDEX_API_KEY, BuildConfig.WEATHER_API_KEY)
             }
         Thread {
             try {
@@ -39,6 +43,8 @@ class WeatherLoader(private val onServerResponseListener: OnServerResponse) {
                 }
             } catch (e: JsonSyntaxException) {
                 Log.d("mylogs", "Ошибка JsonSyntax")
+            } catch (e: FileNotFoundException) {
+                Log.d("mylogs", "Ошибка FileNotFound")
             } finally {
                 urlConnection.disconnect()
             }
--- app/src/main/java/com/example/weatherapp/data/weather_dto/Info.kt
+++ app/src/main/java/com/example/weatherapp/data/weather_dto/Info.kt
@@ -1,7 +1,11 @@
 package com.example.weatherapp.data.weather_dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Info(
     val lat: Double,
     val lon: Double,
     val url: String
-)
+):Parcelable
