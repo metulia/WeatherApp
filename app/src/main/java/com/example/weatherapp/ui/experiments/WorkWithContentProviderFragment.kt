f2bf1c5e37e42a980593765f1d6cdd046a90ed76
Yuliia Stepkina <metulia@mail.ru>
2023-03-29 13:47:02 +0300

    started lesson 9 added WorkWithContentProviderFragment.kt

---
+++ app/src/main/java/com/example/weatherapp/ui/experiments/WorkWithContentProviderFragment.kt
@@ -0,0 +1,37 @@
+package com.example.weatherapp.ui.experiments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weatherapp.databinding.FragmentWorkWithContentProviderBinding
+
+class WorkWithContentProviderFragment : Fragment() {
+
+    private var _binding: FragmentWorkWithContentProviderBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWorkWithContentProviderBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = WorkWithContentProviderFragment()
+    }
+}
